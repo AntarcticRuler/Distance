61776022ab8482a5ba29db75c9f8fdd0409f10b8
AntarcticRuler <34521078+AntarcticRuler@users.noreply.github.com>
2019-12-30 07:44:11 -0500

    Update
    - Added change feature
    - Cleaned up package & node_modules

--- index.js
+++ index.js
@@ -15,15 +15,13 @@ const MongoClient = require("mongodb").MongoClient;
 
 const url = process.env.MONGO_URL;
 
-const dbClient = new MongoClient(url);
+const dbClient = new MongoClient(url, { useUnifiedTopology: true });
 
 var db;
 var collection;
 
 const mpqKey = process.env.MAPQUEST_KEY;
 const axios = require("axios");
-
-const http = require("http");
 
 // Boots up the bot
 client.on("ready", () => {
@@ -34,6 +32,7 @@ client.on("ready", () => {
     db = dbClient.db("Locations");
     collection = db.collection("locations");
     client.user.setActivity(`*register  |  *user`);
+    runMessage();
   });
 });
 
@@ -51,7 +50,9 @@ function registerUser(message) {
     // Checks to see if user is already registered
     if (result.length != 0)
       return message.channel.send(
-        new Discord.RichEmbed().setDescription("You are already registered!")
+        new Discord.RichEmbed().setDescription(
+          "You are already registered!\nUse the change command instead:\n*change Town, State/Province/Country"
+        )
       );
     // Registers user
     else register(message);
@@ -63,13 +64,32 @@ function register(message) {
   console.log("Attempting to register: " + message.author.id);
   var user = {
     id: message.author.id,
-    location: message.content.slice(`${prefix}register`.length).toLowerCase()
+    location: message.content.slice(`${prefix}register `.length).toLowerCase()
   };
   collection.insertOne(user, function(err, res) {
     if (err) throw err;
     console.log("Inserted:\n" + user);
     message.channel.send(new Discord.RichEmbed().setDescription("Registered!"));
   });
+}
+
+// Changes the users town
+function changeTown(message) {
+  let newLocation = message.content
+    .slice(`${prefix}change `.length)
+    .toLowerCase();
+  collection.updateOne(
+    { id: message.author.id },
+    { $set: { location: newLocation } },
+    function(err, res) {
+      if (err) throw err;
+      message.channel.send(
+        new Discord.RichEmbed().setDescription(
+          "Your hometown has been updated!"
+        )
+      );
+    }
+  );
 }
 
 // Posts the towns and gets the request for distances
@@ -92,7 +112,7 @@ function dist(p1, p2, message) {
 // Pulls the request from the database for the author and the target
 function calculateDistance(author, target, message) {
   // Check to see if you are checking your own location
-  if (author.id == target.id)
+  if (author == target)
     return message.channel.send(
       new Discord.RichEmbed().setDescription(
         "You are currently standing in your own location!\nNot much point in giving a distance of 0..."
@@ -103,6 +123,12 @@ function calculateDistance(author, target, message) {
     .find({ $or: [{ id: author }, { id: target }] })
     .toArray(function(err, result) {
       if (err) throw err;
+      if (result.length != 2)
+        return message.channel.send(
+          new Discord.RichEmbed().setDescription(
+            "The other user does not have a hometown set!"
+          )
+        );
       dist(result[0].location, result[1].location, message);
     });
 }
@@ -122,31 +148,52 @@ function returnDistance(res, message) {
 }
 
 const prefix = "*";
-// Checks to see if the !verify command was used on the Peddie Discord server in the specific #verify channel
-client.on("message", message => {
-  // Variables
-  var mention = message.mentions.users.first();
-  let msg = message.content.toLowerCase();
-  // Register User
-  if (msg.startsWith(`${prefix}register`) && message.content.split(" ")[1])
-    registerUser(message);
-  // Register w/o town
-  else if (msg.startsWith(`${prefix}register`))
-    return message.channel.send(
-      new Discord.RichEmbed().setDescription(
-        `When registering, please type:\n*register Town, State/Province/Country`
-      )
-    );
-  // @ User
-  else if (msg.startsWith(`${prefix}user`) && mention)
-    calculateDistance(message.author.id, mention.id, message);
-  // User without @
-  else if (msg.startsWith(`${prefix}user`))
-    return message.channel.send(
-      new Discord.RichEmbed().setDescription(
-        `Please @ a user after the command:\n*user @user`
-      )
-    );
-});
+// Message is inside a function to verify that it only runs once the DB has been connected to
+function runMessage() {
+  client.on("message", message => {
+    // Variables
+    var mention = message.mentions.users.first();
+    let msg = message.content.toLowerCase();
+    // Register User
+    if (msg.startsWith(`${prefix}register`) && message.content.split(" ")[1])
+      registerUser(message);
+    // Register w/o town
+    else if (msg.startsWith(`${prefix}register`))
+      return message.channel.send(
+        new Discord.RichEmbed().setDescription(
+          `When registering, please type:\n*register Town, State/Province/Country`
+        )
+      );
+    // Change town
+    else if (msg.startsWith(`${prefix}change`) && message.content.split(" ")[1])
+      changeTown(message);
+    // Change town w/o town
+    else if (msg.startsWith(`${prefix}change`))
+      return message.channel.send(
+        new Discord.RichEmbed().setDescription(
+          `When changing your town, please type:\n*register Town, State/Province/Country`
+        )
+      );
+    // @ User
+    else if (msg.startsWith(`${prefix}user`) && mention)
+      calculateDistance(message.author.id, mention.id, message);
+    // User without @
+    else if (msg.startsWith(`${prefix}user`))
+      return message.channel.send(
+        new Discord.RichEmbed().setDescription(
+          `Please @ a user after the command:\n*user @user`
+        )
+      );
+    // Help command
+    else if (msg.startsWith(`${prefix}help`))
+      return message.channel.send(
+        new Discord.RichEmbed()
+          .setDescription(
+            `**Registration**\n${prefix}register Town, State/Province/Country\n\n**Change Town**\n${prefix}change Town, State/Province/Country\n\n**Distance to user**\n${prefix}user @user\n\nhttp://www.nick-studios.com/discord-location`
+          )
+          .setAuthor(`Discord Location Commands`, client.user.avatarURL)
+      );
+  });
+}
 
 client.login(token);
